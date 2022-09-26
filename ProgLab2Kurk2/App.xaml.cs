4c54f0d888f9ed0c7daac051cd80e729fd380255
Tus <tusxdie@gmail.com>
2022-09-26 13:45:41 +0700

    init commit

---
+++ ProgLab2Kurk2/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ProgLab2Kurk2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ ProgLab2Kurk2/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace ProgLab2Kurk2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
